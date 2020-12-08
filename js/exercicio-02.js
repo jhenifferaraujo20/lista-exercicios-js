c8e79ac2ff8433a832198de3d530789baa2ff12a
Jheniffer Araujo <jhenifferbaldi@gmail.com>
2020-12-08 20:15:54 -0300

    exercicio 02

---
+++ js/exercicio-02.js
@@ -0,0 +1,21 @@
+function Soma(){
+    let numero1 = document.getElementById("numero1").value;
+    let numero2 = document.getElementById("numero2").value;
+    //float é tipo de dado decimal
+    let conta = parseFloat(numero1) + parseFloat(numero2);
+
+    //document.getElementById("resultado").innerText = conta;
+
+    if(conta == 0)
+    {
+        document.getElementById("resultado").innerText = "Neutro";
+    }
+    else if(conta > 0)
+    {
+        document.getElementById("resultado").innerText = "Positivo";
+    }
+    else
+    {
+        document.getElementById("resultado").innerText = "Negativo";
+    }
+}
